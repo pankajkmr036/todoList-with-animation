4f1f1a279b4d2cb07a24a9185004a2938f41dc55
Pankaj Kumar <pankajk@orowealth.com>
2021-12-10 22:51:05 +0530

    Project intitialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>TODO LIST APP</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
