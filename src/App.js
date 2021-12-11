efe3ddc7455ab4ea8cc8278b5826971e392a266a
Pankaj Kumar <pankajk@orowealth.com>
2021-12-11 12:48:27 +0530

    react-native paper setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, Text} from 'react-native';
 import {Provider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 import store from './redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Text>TODO LIST APP</Text>
-        </ScrollView>
-      </SafeAreaView>
+      <PaperProvider>
+        <SafeAreaView>
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <Text>TODO LIST APP</Text>
+          </ScrollView>
+        </SafeAreaView>
+      </PaperProvider>
     </Provider>
   );
 };
